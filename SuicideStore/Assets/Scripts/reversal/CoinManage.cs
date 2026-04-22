a11edc0c0964cbd2305147de8caee7fa4fe9d817
HYXiaoHai <HYXiaoHai@users.noreply.github.com>
2026-04-22 11:28:13 +0800

    换素材

--- SuicideStore/Assets/Scripts/reversal/CoinManage.cs
+++ SuicideStore/Assets/Scripts/reversal/CoinManage.cs
@@ -11,8 +11,8 @@ public class CoinManage : MonoBehaviour
     public List<Coin>coins = new List<Coin>();               // 场景中所有金币（可手动拖拽，也可自动查找）
 
     [Header("偏移设置")]
-    public Vector3 baseOffset = new Vector3(-1.5f, 0f, 0f);   // 金币1相对于玩家的偏移（如左侧1.5单位）
-    public Vector3 chainOffset = new Vector3(-0.5f, 0f, 0f);  // 后续金币相对于前一个金币的额外偏移（可选）
+    public Vector3 baseOffset;   // 金币1相对于玩家的偏移（如左侧1.5单位）
+    public Vector3 chainOffset;  // 后续金币相对于前一个金币的额外偏移（可选）
 
     private Vector3 originalBaseOffset;
     private Vector3 originalChainOffset;
@@ -24,14 +24,13 @@ public class CoinManage : MonoBehaviour
     }
     void Start()
     {
+        originalBaseOffset = baseOffset;
+        originalChainOffset = chainOffset;
+        Debug.Log(originalBaseOffset + " " + originalChainOffset);
         if (coins.Count == 0)
         {
             return;
         }
-
-        originalBaseOffset = baseOffset;
-        originalChainOffset = chainOffset;
-
         //初始朝向为右，使用方向 = 1
         originalDic = 1;
         SetupChains(1);
@@ -40,7 +39,6 @@ public class CoinManage : MonoBehaviour
     {
         // 方向为 1（右）时偏移保持原样（负X在左边）
         // 方向为 -1（左）时偏移X取反（正X在右边）
-        Debug.Log(originalBaseOffset + " " + originalChainOffset);
         Vector3 actualBaseOffset = new Vector3(baseOffset.x * direction, originalBaseOffset.y, originalBaseOffset.z);
         Vector3 actualChainOffset = new Vector3(chainOffset.x * direction, originalChainOffset.y, originalChainOffset.z);
         for (int i = 0; i < coins.Count; i++)
@@ -64,7 +62,7 @@ public class CoinManage : MonoBehaviour
     public void AddCoin(Coin coin)
     {
         coins.Add(coin);
-        UpdateDirection(originalDic);
+        //UpdateDirection(originalDic);
     }
     public void UpdateDirection(int direction)
     {
