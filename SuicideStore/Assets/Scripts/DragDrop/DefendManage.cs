37f900e564812574da09ef97de905058d1b432ea
HYXiaoHai <HYXiaoHai@users.noreply.github.com>
2026-05-18 15:10:08 +0800

    还有这个

--- SuicideStore/Assets/Scripts/DragDrop/DefendManage.cs
+++ SuicideStore/Assets/Scripts/DragDrop/DefendManage.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections;
-using UnityEditor.ShaderGraph.Internal;
 
 public class DefendManage : MonoBehaviour
 {
--- SuicideStore/Assets/Scripts/Scene10/Scene10Manage.cs
+++ SuicideStore/Assets/Scripts/Scene10/Scene10Manage.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class Scene10Manage : MonoBehaviour
 {
